096974a94e9c40a32059158adbb951ecdde754de
Cenk Bahadir Cark <79763515+cenkbcark@users.noreply.github.com>
2023-04-02 13:19:34 +0300

    Initial Commit

---
+++ QRScannerSwiftUI/QRScannerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerSwiftUIApp.swift
+//  QRScannerSwiftUI
+//
+//  Created by Cenk Bahadır Çark on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
